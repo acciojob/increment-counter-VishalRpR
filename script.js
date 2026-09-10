58431591a1951c7a7e214473f61c44e727587f43
Vishal <120316470+VishalRpR@users.noreply.github.com>
2026-09-10 13:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+     let value=document.getElementById("counter")
+		
+		let btn =document.getElementById("incrementBtn").onclick=clicked
+		function clicked(){
+           alert("you clicked the button")
+			value.innerText=Number(value.innerText)+1
+		
+		}
